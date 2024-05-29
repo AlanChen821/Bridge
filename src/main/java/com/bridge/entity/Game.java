d1a2ac975c9f9816a7f78ea7833fd30f36d26c26
Alan <sgalan821@gmail.com>
2024-05-29 21:40:08 +0800

    feat-alan-login : 1. create a new game when no waiting room to enter. 2. can enter a waiting game.

--- src/main/java/com/bridge/entity/Game.java
+++ src/main/java/com/bridge/entity/Game.java
@@ -12,6 +12,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -22,7 +23,17 @@ import java.util.List;
 public class Game {
 
     public Game() {
+        this.gameId = "1";
+        this.players = new ArrayList<>();
         this.createTime = LocalDateTimeUtils.getStringOfNow();
+        this.status = GameStatus.WAITING;
+
+    }
+
+    public Game(Player firstPlayer) {
+        this();
+
+        this.players.add(firstPlayer);
     }
 
     private String gameId;
@@ -34,4 +45,11 @@ public class Game {
     private GameStatus status;
     private String createTime;
     private String updateTime;
+
+    public void addNewPlayer(Player newPlayer) {
+        this.players.add(newPlayer);
+        if (this.players.size() == 4) {
+            this.setStatus(GameStatus.CALLING);
+        }
+    }
 }
