52fc695c7ae172de3bf0e631d08b0bbdd9e1a033
AlanChen821 <30295481+AlanChen821@users.noreply.github.com>
2025-03-02 09:51:27 +0800

    feat-alan-jdbc : integrate with MySQL & PostgreSQL jdbc. (#33)

--- src/main/java/com/bridge/BridgeApplication.java
+++ src/main/java/com/bridge/BridgeApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication
 @Slf4j
 @MapperScan("com.bridge.mapper")
 public class BridgeApplication {
