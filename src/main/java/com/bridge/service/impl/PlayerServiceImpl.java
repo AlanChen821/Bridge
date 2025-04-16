6132600bfecf3e3aa53457c775f89876ff519b17
Alan <sgalan821@gmail.com>
2025-04-16 21:39:10 +0800

    feat-alan-jwt : 1. add jwt library. can generate jwt token when register and verify with it in other APIs. 2. [POST] /players : only NORMAL users can register.

--- src/main/java/com/bridge/service/impl/PlayerServiceImpl.java
+++ src/main/java/com/bridge/service/impl/PlayerServiceImpl.java
@@ -4,6 +4,7 @@ import com.bridge.entity.user.Player;
 import com.bridge.enumeration.PlayerType;
 import com.bridge.mapper.PlayerMapper;
 import com.bridge.service.IPlayerService;
+import com.bridge.utils.JwtUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -15,17 +16,27 @@ public class PlayerServiceImpl implements IPlayerService {
 
     private final PlayerMapper playerMapper;
 
-    public PlayerServiceImpl(PlayerMapper playerMapper) {
+    private final JwtUtil jwtUtil;
+
+    public PlayerServiceImpl(PlayerMapper playerMapper, JwtUtil jwtUtil) {
         this.playerMapper = playerMapper;
+        this.jwtUtil = jwtUtil;
     }
 
     @Override
     public Player register(Player player) {
         if (player.getType().equals(PlayerType.USER.getCode())) {
+            //  only when a normal user registers need to encrypt the password and insert into db
             player.encryptPassword();
+            playerMapper.insertPlayer(player);
+            String token = jwtUtil.generateToken(player);
+            log.info("Player {} token : {}", player.getAccount(), token);
+            player.setToken(token);
+            return player;
+        } else {
+            //  TEST user shouldn't be able to register
+            return null;
         }
-        playerMapper.insertPlayer(player);
-        return player;
     }
 
     @Override
